d0f461aabad576875cd336f44e7e507b888df13a
Bernard Meunier <beurnii@gmail.com>
2020-04-21 20:46:55 -0400

    wouhou

--- projet/NN.py
+++ projet/NN.py
@@ -1,16 +1,27 @@
 import numpy as np
 import random
+import copy
+
 
 class NN:
-    def __init__(self, input_nodes, hidden_layers, output_nodes):
-        self.W = []
+    def __init__(self, w):
+        self.W = w
+
+    @classmethod
+    def from_params(cls, input_nodes, hidden_layers, output_nodes):  # NN.from_params(3,[3,4,5],1)
+        w = []
         if len(hidden_layers):
-            self.W.append(np.random.normal(0, 0.1, (input_nodes+1, hidden_layers[0])))
+            w.append(np.random.normal(0, 0.1, (input_nodes + 1, hidden_layers[0])))
             for i in range(1, len(hidden_layers)):
-                self.W.append(np.random.normal(0, 0.1, (hidden_layers[i-1]+1, hidden_layers[i])))
-            self.W.append(np.random.normal(0, 0.1, (hidden_layers[-1]+1, output_nodes)))
+                w.append(np.random.normal(0, 0.1, (hidden_layers[i - 1] + 1, hidden_layers[i])))
+            w.append(np.random.normal(0, 0.1, (hidden_layers[-1] + 1, output_nodes)))
         else:
-            self.W.append(np.random.normal(0, 0.1, (input_nodes+1, output_nodes)))
+            w.append(np.random.normal(0, 0.1, (input_nodes + 1, output_nodes)))
+        return cls(w)
+
+    @classmethod
+    def from_weights(cls, w):
+        return cls(w)
 
     def hidden_activation(self, Z):
         return np.maximum(Z, 0)
@@ -21,26 +32,30 @@ class NN:
 
     def predict(self, inputs):
         a = np.append(np.array(inputs), 1)
-        for i in range(len(self.W)-1):
+        for i in range(len(self.W) - 1):
             Z = np.squeeze(a @ self.W[i])
             a = np.append(self.hidden_activation(Z), 1)
         Z = np.squeeze(a @ self.W[-1])
         a = self.softmax_activation(Z)
         return np.argmax(a)
 
-    def copy(self):
-        pass
+    def get_copy(self):
+        _w = []
+        for w in self.W:
+            _w.append(np.copy(w))
+
+        return _w
 
     def crossover(self):
         pass
 
     def mutate(self, rate):
-        newW = []
+        _w = []
         for m in self.W:
-            newM = np.copy(m)
-            newW.append(newM)
-            for w in np.nditer(newM, op_flags=['readwrite']):
+            new_m = np.copy(m)
+            for w in np.nditer(new_m, op_flags=['readwrite']):
                 if random.random() < rate:
                     w[...] = random.random()
-        return newW
+            _w.append(new_m)
 
+        return _w
--- projet/bench.py
+++ projet/bench.py
@@ -1,7 +1,24 @@
 import numpy as np
+import random
+import operator
 
-a = None
-inputs = [1,2,3,4]
-a = np.append(np.array(inputs), 1)
+class Agent:
+    def __init__(self):
 
-print(a)
+        self.reward = random.random()*100
+        # if w:
+        #     self.NN = NN.from_weights(w)
+        # else:
+        #     self.NN = NN.from_params(self.observation_size, [2], self.action_size)
+
+    def get_action(self, state):
+        pole_angle = state[2]
+        action = 0 if pole_angle < 0 else 1
+        return action
+
+    def set_reward(self, reward):
+        self.reward = reward
+
+agents = [Agent() for i in range(50)]
+agents.sort(key=operator.attrgetter('reward'))
+breakpoint()
