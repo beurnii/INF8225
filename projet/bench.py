51ebb56df8797fd61e97e93017714d6f8f692929
Bernard Meunier <beurnii@gmail.com>
2020-04-26 17:12:35 -0400

    encore un peu plus

--- projet/bench.py
+++ projet/bench.py
@@ -2,4 +2,10 @@ import numpy as np
 import random
 import operator
 
-print(49**0.5)
+a = np.array([[0,1,2],[3,4,5],[6,7,8],[1,2,3]])
+b = np.array([[10,11,12],[13,41,51],[16,17,18],[1,2,43]])
+
+c = np.copy(a.T)
+c[1] = b.T[1]
+c = c.T
+breakpoint()
